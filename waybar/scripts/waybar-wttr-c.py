aafb0841552f27fbd05a3d8a7b5054bf3263e875
Divyaprakash <divyaprakash.0426@gmail.com>
2024-11-19 04:54:15 +0530

    fix: Add extrachar for single-digit positive temperatures

--- waybar/scripts/waybar-wttr-c.py
+++ waybar/scripts/waybar-wttr-c.py
@@ -164,7 +164,7 @@ if tempint > 0 and tempint < 10:
     extrachar = '+'  # Add plus sign for positive single digits
 
 data['text'] = ' '+get_weather_emoji(weather['current']['weather'][0]['id']) + \
-    " "+str(round(weather['current']['feels_like']))+"°"
+    " "+extrachar+str(round(weather['current']['feels_like']))+"°"
 
 # Build detailed tooltip with current conditions and forecast
 aqi = get_aqi(lat, lon)
